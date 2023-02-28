bdd94e7df2e073c32b15fd8b87fb5f564769bc33
高杉宗汰 <sotatk25@KetsuzeiMacBook-Air.local>
2023-02-28 23:57:01 +0900

    login実装_securityなし

---
+++ src/main/java/com/example/demo/common/KintaiConstants.java
@@ -0,0 +1,18 @@
+package com.example.demo.common;
+
+public class KintaiConstants {
+
+	/*******
+	 * URL *
+	 *******/
+
+	/** ログインページ */
+	public static final String LOGIN_URL = "/login";
+
+	/**
+	 * 遷移先パス
+	 */
+
+	/** ログイン画面のパス */
+	public static final String LOGIN_PASS = "user/login";
+}
